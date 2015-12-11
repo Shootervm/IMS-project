92ece0d1ec3c9dcc17779b3865260ff4ecb8b84d
Vojtech Mašek <shooter.vm@gmail.com>
2015-12-11 19:55:35 +0100

    basic simulation in main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <simlib.h>
 
 using namespace std;
 
+Facility Cashier("Cashier");
+Store Fooder("Fooder", 2);
+Facility Soaper("Soaper");
+
+const double kCustomerArrivalTime = 1e3 / 150;
+
+class Customer : public Process {
+
+    void Behavior() { }
+};
+
+class CustomerGenerator : public Event {
+public:
+    void Behavior() {
+        (new Customer)->Activate();
+        Activate(Time + Exponential(kCustomerArrivalTime));
+    }
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+
+    Init(0, 1000);
+
+    (new CustomerGenerator)->Activate();
+    Run();
+
+
     return 0;
 }
