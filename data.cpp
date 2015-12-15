605010fe0cfc67d7d65d58935ae6e6f7a836bbae
Vojtech Mašek <shooter.vm@gmail.com>
2015-12-15 15:13:03 +0100

    sim. data

---
+++ data.cpp
@@ -0,0 +1,78 @@
+#include "data.h"
+
+ExperimentData peak = {
+        60 * 60 * 3, // simulationLength
+        17.0, // kCustomerArrivalTime
+        4.65, // kTrayStandProcessTime
+        5.2, // kSoupTakeTime
+        10.0, // kPizzaOrderTime
+        6.0, // kPreparedFoodTakeTime
+        16.0, // kUnpreparedFoodTakeTime
+        7.3, // kWaterTakeTime
+        5.5, // kETCTakeTime
+        15.0, // kPaymentByCashTime
+        6.5, // kPaymentByISICTime
+        1080.0, // kEatingTime
+        4.0 // kReturnTrayProcessTime
+};
+ExperimentData stable = {
+        60 * 60 * 3, // simulationLength
+        17.0, // kCustomerArrivalTime
+        4.65, // kTrayStandProcessTime
+        5.2, // kSoupTakeTime
+        10.0, // kPizzaOrderTime
+        6.0, // kPreparedFoodTakeTime
+        16.0, // kUnpreparedFoodTakeTime
+        7.3, // kWaterTakeTime
+        5.5, // kETCTakeTime
+        15.0, // kPaymentByCashTime
+        6.5, // kPaymentByISICTime
+        1080.0, // kEatingTime
+        4.0 // kReturnTrayProcessTime
+};
+ExperimentData closing = {
+        60 * 60 * 3, // simulationLength
+        17.0, // kCustomerArrivalTime
+        4.65, // kTrayStandProcessTime
+        5.2, // kSoupTakeTime
+        10.0, // kPizzaOrderTime
+        6.0, // kPreparedFoodTakeTime
+        16.0, // kUnpreparedFoodTakeTime
+        7.3, // kWaterTakeTime
+        5.5, // kETCTakeTime
+        15.0, // kPaymentByCashTime
+        6.5, // kPaymentByISICTime
+        1080.0, // kEatingTime
+        4.0 // kReturnTrayProcessTime
+};
+ExperimentData typical = {
+        60 * 60 * 3, // simulationLength
+        17.0, // kCustomerArrivalTime
+        4.65, // kTrayStandProcessTime
+        5.2, // kSoupTakeTime
+        10.0, // kPizzaOrderTime
+        6.0, // kPreparedFoodTakeTime
+        16.0, // kUnpreparedFoodTakeTime
+        7.3, // kWaterTakeTime
+        5.5, // kETCTakeTime
+        15.0, // kPaymentByCashTime
+        6.5, // kPaymentByISICTime
+        1080.0, // kEatingTime
+        4.0 // kReturnTrayProcessTime
+};
+
+ExperimentChances chances = {
+        .kCustomerWithSoup = 30.0,
+        .kCustomerFoodPizza = 15.0,
+        .kCustomerFoodPrepared = 30.0,
+        .kCustomerWithWater = 65.0,
+        .kCustomerWithETC = 40.0,
+        .kPaymentByCash = 25.0
+};
+
+ExperimentData data = typical;
+
+ExperimentEnvironment environment;
+
+
+
---
+++ data.h
@@ -0,0 +1,52 @@
+
+#ifndef IMS_PROJECT_DATA_H
+#define IMS_PROJECT_DATA_H
+
+typedef struct {
+    unsigned int kCapacity = 120,
+            kQueueQuitLength = 25,
+            kQueueQuitChance = 15,
+            quited = 0;
+} ExperimentEnvironment;
+
+
+typedef struct {
+    double kCustomerWithSoup,
+            kCustomerFoodPizza,
+            kCustomerFoodPrepared,
+            kCustomerWithWater,
+            kCustomerWithETC,
+            kPaymentByCash;
+} ExperimentChances;
+
+
+typedef struct {
+    unsigned int simulationLength;
+    double kCustomerArrivalTime,
+            kTrayStandProcessTime,
+            kSoupTakeTime,
+            kPizzaOrderTime,
+            kPreparedFoodTakeTime,
+            kUnpreparedFoodTakeTime,
+            kWaterTakeTime,
+            kETCTakeTime,
+            kPaymentByCashTime,
+            kPaymentByISICTime,
+            kEatingTime,
+            kReturnTrayProcessTime;
+} ExperimentData;
+
+extern ExperimentData peak;
+extern ExperimentData stable;
+extern ExperimentData closing;
+extern ExperimentData typical;
+extern ExperimentData data;
+
+extern ExperimentChances chances;
+
+extern ExperimentEnvironment environment;
+
+#endif //IMS_PROJECT_DATA_H
+
+
+
